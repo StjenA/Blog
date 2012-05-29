32d7dceaf401f1d345786c2516a41abcf77e01fb
Auke van Scheltinga <aukevs@gmail.com>
2012-05-29 15:28:31 +0200

    added translations

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,16 @@
+class HomeController < ApplicationController
+  
+  def index
+    @username = params[:username]
+    @password = params[:password]
+  end
+  
+  def show
+    
+  end
+  
+  def destroy
+    
+  end
+  
+end
